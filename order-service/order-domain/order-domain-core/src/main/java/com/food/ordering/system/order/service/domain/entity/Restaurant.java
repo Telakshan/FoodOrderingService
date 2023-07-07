9fa6ceda272b4c8c283c7d5fd77f2af7ea36d107
Thilakshan Baskaran <telakshanb@gmail.com>
2023-07-07 12:06:11 -0400

    add interface

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Restaurant.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Restaurant.java
@@ -9,11 +9,49 @@ public class Restaurant extends AggregateRoot<RestaurantId> {
     private final List<Product> products;
     private boolean active;
 
+    private Restaurant(Builder builder) {
+        super.setId(builder.restaurantId);
+        products = builder.products;
+        active = builder.active;
+    }
+
+    public static  Builder builder(){
+        return new Builder();
+    }
+
     public List<Product> getProducts() {
         return products;
     }
 
     public boolean isActive() {
         return active;
+    }
+
+    public static final class Builder {
+        private RestaurantId restaurantId;
+        private List<Product> products;
+        private boolean active;
+
+        public Builder(){
+        }
+
+        public Builder Id(RestaurantId val) {
+            restaurantId = val;
+            return this;
+        }
+
+        public Builder products(List<Product> val){
+            products = val;
+            return this;
+        }
+
+        public Builder active(boolean val) {
+            active = val;
+            return this;
+        }
+
+        public Restaurant build() {
+            return new Restaurant(this);
+        }
     }
 }
