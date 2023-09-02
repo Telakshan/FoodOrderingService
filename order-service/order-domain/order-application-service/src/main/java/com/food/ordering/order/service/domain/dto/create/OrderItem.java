d120e4dfd72a74b0ddaf9417d6eeafae5978a055
Thilakshan Baskaran <telakshanb@gmail.com>
2023-09-02 16:58:13 -0400

    add dto for application-service side

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/order/service/domain/dto/create/OrderItem.java
@@ -0,0 +1,19 @@
+package com.food.ordering.order.service.domain.dto.create;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class OrderItem {
+    private final UUID productId;
+    private final Integer quantity;
+    private final BigDecimal price;
+    private final BigDecimal subTotal;
+}
+
